b857621654be08b4307e101ba63ec99e4e68ccea
Shem <smagnezi8@gmail.com>
2022-11-23 08:35:40 +0000

    trigger

--- backend/lib/openai.js
+++ backend/lib/openai.js
@@ -2,6 +2,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 const createImage = async (text) => {
